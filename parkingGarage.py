ed8601ca2d1fdb30dd3698824e54ddbd62678818
unknown <cwilliams.byui@gmail.com>
2022-02-19 11:50:44 -0700

    first commit

---
+++ parkingGarage.py
@@ -0,0 +1,24 @@
+# Parking Garage Project
+
+# Methods
+    # takeTicket
+        # decrease amount of tickets by 1
+        # decrease parkingSpace by 1
+    
+    # payForParking
+        # display input with amount from user and stores it
+        # if payment variable is not empty display that their ticket has been paid and they have 15 mins to leave
+        # update currentTicket dictionary key "paid" to True
+
+    # leaveGarage
+        # if ticket paid display "Thank you, have a nice day."
+        # if not paid input prompt for payment 
+            # once paid display "Thank you, have a nice day"
+        # update parkingSpaces list to increase by 1, (meaning add to the parkingSpaces list)
+        # update ticket list to increase by 1, (meaning add to the tickets list)
+
+# Attributes required
+    # tickets - list
+    # parkingSpace - list
+    # currentTicket - dictionary
+
