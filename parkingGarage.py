8c7f11a9e0a147e00b90a37e46bddbe838a67fad
unknown <cwilliams.byui@gmail.com>
2022-02-19 21:19:30 -0700

    cleaned up comments

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,37 +1,9 @@
 # Parking Garage Project
 
-# Methods
-    # takeTicket
-        # show availavle spots
-        # select spot
-        # decrease amount of tickets by 1
-        # decrease parkingSpace by 1
-    
-    # payForParking
-        # display input with amount from user and stores it
-        # if payment variable is not empty display that their ticket has been paid and they have 15 mins to leave
-        # update currentTicket dictionary key "paid" to True
-
-    # leaveGarage
-        # if ticket paid display "Thank you, have a nice day."
-        # if not paid input prompt for payment 
-            # once paid display "Thank you, have a nice day"
-        # update parkingSpaces list to increase by 1, (meaning add to the parkingSpaces list)
-        # update ticket list to increase by 1, (meaning add to the tickets list)
-
-# Attributes required
-    # tickets - list
-    # parkingSpace - list
-    # currentTicket - dictionary
-
-# Added some text here ------------------------------------
-
-
+# imports function from another file to have a cool car image 
 import car_image
 image = car_image
 image.playImage()
-
-
 
 class ParkingGarage():
 
@@ -57,31 +29,30 @@ class ParkingGarage():
             # parkingSpaces reduce by 1
             print(self.parkingSpace.pop(0))
         else:
+            # print message if no tickets available
             print("Garage is full, Thank you!")
 
 
     def payForParking(self):
-        # prompt for value (ticket number) store in payment variable
+        # prompt for value (ticket number) to store in payment variable
         spot = int(input('What is your ticket number? '))
         print(type(spot))
         
         
         # if payment variable is not empty (ticket has been paid) display message: 15 minutes to leave
-        if self.currentTicket.get(spot)== "unpaid": 
+        if self.currentTicket.get(spot) == "unpaid": 
             print("You're ticket has been paid, you have 15 minutes to leave.")
             # update currentTicket dict key "paid" to true
-            self.currentTicket[spot]= 'paid'
+            self.currentTicket[spot] = 'paid'
 
             print(self.currentTicket)
-        # update update parkingSpaces list to increase by 1
+            # update update parkingSpaces list to increase by 1
             self.parkingSpace.append(spot)
-        
-
 
     def leaveGarage(self):
         # if ticket paid, dict value == True display: Thank you, have a nice day
         vacant = int(input('What is your ticket number? '))
-            # when paid display: Thank you, have a nice day
+        # when paid display: Thank you, have a nice day
         if self.currentTicket[vacant] == "paid":
             image.playImage()
             print("                Thank you have a nice day.")
@@ -89,32 +60,33 @@ class ParkingGarage():
             self.tickets.append(vacant)
             print(self.tickets)
             print(self.parkingSpace)
-        # if not paid display: input for payment
+        # if not paid display: message to pay ticket before leaving
         elif self.currentTicket[vacant] == "unpaid":
-            print("enter payment information")
+            print("You must pay your ticket before leaving.")
         
 
-
+    # menu for calling all methods defined in our class
     def runParkingGarage(self):
         while True:
             response = input('What do you want to do? (T)Take Ticket/ (P)Pay for Ticket/ (L)Leave Garage/ (Q)Quit > ')
             
-
-            if response.lower().strip() == 'q':          
+            # Quit
+            if response.lower().strip() == 'q':       
                 image.playImage()
                 print('                Thanks for your interest, we look forward to your business.')
                 break
+            # Take ticket
             elif response.lower().strip() == 't':
                 ParkingGarage.takeTicket(self)
-                pass
+            # Pay for ticket
             elif response.lower().strip() == 'p':
-                ParkingGarage.payForParking(self)
-                pass   
+                ParkingGarage.payForParking(self) 
+            # Leave Garage
             elif response.lower().strip() == 'l':
                 ParkingGarage.leaveGarage(self)
-                pass
 
         return
 
+# runs the program
 run = ParkingGarage()
 run.runParkingGarage()
